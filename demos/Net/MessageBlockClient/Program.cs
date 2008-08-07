96f6e6965d7b7ff3bce4e5e6e7e2cca17d265d33
Sjteksoft@gmail.com <Sjteksoft@gmail.com@fbfec8fb-a053-0410-97e8-c5a9e5c66af0>
2008-08-07 09:29:59 +0000

    完善Net库演示程序，添加MessageBlockClient客户羰程序。

--- demos/Net/MessageBlockClient/Program.cs
+++ demos/Net/MessageBlockClient/Program.cs
@@ -1,14 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
+
+using Uniframework.Net;
 
 namespace MessageBlockClient
 {
     class Program
     {
+        private static MessageClient client = null;
+
         static void Main(string[] args)
         {
+            Trace.Listeners.Add(new ConsoleTraceListener());
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+
+            using (MessageClient client = new MessageClient())
+            {
+                client.Host = "127.0.0.1";
+                client.Port = 8088;
+                client.HeartBeatPeriod = 10000; //10 second
+                client.Start();
+                Program.client = client;
+
+                Console.WriteLine("Press enter to exit...");
+                Thread.Sleep(1000);
+                Thread t1 = new Thread(SendData1);
+                t1.Start();
+                Thread t2 = new Thread(SendData2);
+                t2.Start();
+                Console.ReadLine();
+            }
+            Console.ReadLine();
+        }
+
+        static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Console.WriteLine(e.ExceptionObject.ToString());
+        }
+
+        static void SendData1()
+        {
+            while (client.IsConnected)
+            {
+                string data = "hello from thread 1";
+                client.Send(new MessageBlock(Encoding.Unicode.GetBytes(data)));
+                Thread.Sleep(1000);
+            }
+        }
+
+        static void SendData2()
+        {
+            while (client.IsConnected)
+            {
+                string data = "hello from thread 2";
+                client.Send(new MessageBlock(Encoding.Unicode.GetBytes(data)));
+                Thread.Sleep(1000);
+            }
         }
     }
 }
