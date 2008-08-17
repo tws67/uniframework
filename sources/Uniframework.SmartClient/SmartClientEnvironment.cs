d5dd676b30c63233f5a0c76d91c34a29c0c22362
Sjteksoft@gmail.com <Sjteksoft@gmail.com@fbfec8fb-a053-0410-97e8-c5a9e5c66af0>
2008-08-17 14:11:25 +0000

    1、修改框架对于权限处理的接口，并修改了CompositeUI中相关的文件； 2、为客户端项目添加图像、图标服务。

---
+++ sources/Uniframework.SmartClient/SmartClientEnvironment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Practices.CompositeUI;
+
+namespace Uniframework.SmartClient
+{
+    /// <summary>
+    /// Uniframework 客户端执行环境
+    /// </summary>
+    [Service]
+    public class SmartClientEnvironment
+    {
+        /// <summary>
+        /// Gets the application path.
+        /// </summary>
+        /// <value>The application path.</value>
+        public string ApplicationPath {
+            get {
+                return FileUtility.ApplicationRootPath;
+            }
+        }
+    }
+}
