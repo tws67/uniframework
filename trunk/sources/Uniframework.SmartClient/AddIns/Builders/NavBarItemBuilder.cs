0dde08be96631ab891f5e4cec6fe5059d511d743
Sjteksoft@gmail.com <Sjteksoft@gmail.com@fbfec8fb-a053-0410-97e8-c5a9e5c66af0>
2008-08-27 09:41:37 +0000

    为XtraForm添加UIBuilder……

---
+++ trunk/sources/Uniframework.SmartClient/AddIns/Builders/NavBarItemBuilder.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using DevExpress.XtraNavBar;
+using Microsoft.Practices.CompositeUI;
+
+namespace Uniframework.SmartClient.AddIns.Builders
+{
+    /// <summary>
+    /// Xtra Navbar Control item builder
+    /// </summary>
+    public class NavBarItemBuilder : IBuilder
+    {
+        #region IBuilder Members
+
+        /// <summary>
+        /// 条件处理标志
+        /// </summary>
+        /// <value></value>
+        public bool HandleConditions
+        {
+            get { return true; }
+        }
+
+        /// <summary>
+        /// 构建器所处理的类的名称
+        /// </summary>
+        /// <value></value>
+        public string ClassName
+        {
+            get { return "NavBarItem"; }
+        }
+
+        /// <summary>
+        /// 构建插件单元
+        /// </summary>
+        /// <param name="caller">调用者</param>
+        /// <param name="context">上下文，用于存放在构建时需要的组件</param>
+        /// <param name="element">插件单元</param>
+        /// <param name="subItems">被构建的子对象列表</param>
+        /// <returns>构建好的插件单元</returns>
+        public object BuildItem(object caller, WorkItem context, AddInElement element, ArrayList subItems)
+        {
+            if (element.Configuration.Attributes["label"] == null)
+                throw new AddInException(String.Format("没有为类型为 \"{0}\" 的插件单元{1}提供label属性。",
+                    element.ClassName, element.Id));
+
+            string label = element.Configuration.Attributes["label"];
+            NavBarItem item = new NavBarItem(label);
+            if (element.Configuration.Attributes["tooltip"] != null)
+                item.Hint = element.Configuration.Attributes["tooltip"];
+
+            if (!String.IsNullOrEmpty(element.Path) && context.UIExtensionSites.Contains(element.Path))
+                context.UIExtensionSites[element.Path].Add(item);
+            return item;
+        }
+
+        #endregion
+    }
+}
