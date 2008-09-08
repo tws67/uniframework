cc2663330acbff3f32dd17d31a93bf7a78efc3ab
Sjteksoft@gmail.com <Sjteksoft@gmail.com@fbfec8fb-a053-0410-97e8-c5a9e5c66af0>
2008-09-08 15:45:22 +0000

    1、修复CAB原有的WindowWorkspace存在的BUG； 2、为WorkItemController添加ShowViewInWorkspace方法。

---
+++ sources/cab/CompositeUI.Common/Workspaces/MdiWorkspace.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+using Microsoft.Practices.CompositeUI.SmartParts;
+using Microsoft.Practices.CompositeUI.WinForms;
+
+namespace Microsoft.Practices.CompositeUI.Common.Workspaces
+{
+    public class MdiWorkspace : WindowWorkspace
+    {
+        private Form parentMdiForm;
+
+        /// <summary>
+        /// Constructor specifying the parent form of the MDI child.
+        /// </summary>
+        public MdiWorkspace(Form parentForm)
+            : base()
+        {
+            this.parentMdiForm = parentForm;
+            this.parentMdiForm.IsMdiContainer = true;
+        }
+
+        /// <summary>
+        /// Gets the parent MDI form.
+        /// </summary>
+        public Form ParentMdiForm
+        {
+            get { return parentMdiForm; }
+        }
+
+        /// <summary>
+        /// Shows the form as a child of the specified <see cref="ParentMdiForm"/>.
+        /// </summary>
+        /// <param name="smartPart">The <see cref="Control"/> to show in the workspace.</param>
+        /// <param name="smartPartInfo">The information to use to show the smart part.</param>
+        protected override void OnShow(Control smartPart, WindowSmartPartInfo smartPartInfo)
+        {
+            Form mdiChild = this.GetOrCreateForm(smartPart);
+            mdiChild.MdiParent = parentMdiForm;
+
+            this.SetWindowProperties(mdiChild, smartPartInfo);
+            mdiChild.Show();
+            this.SetWindowLocation(mdiChild, smartPartInfo);
+            mdiChild.BringToFront();
+        }
+    }
+}
