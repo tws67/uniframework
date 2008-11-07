a57a4b27689e7015d99b7a3d02dcc6129e1f6fbf
Sjteksoft <Sjteksoft@fbfec8fb-a053-0410-97e8-c5a9e5c66af0>
2008-11-07 09:40:14 +0000

    1、更新db4o为7.4稳定版； 2、添加Lephon数据访问组件，官方地址http://www.codeplex.com/dbentry

---
+++ trunk/sources/Uniframework/Service/IDataListService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Uniframework.Services
+{
+    /// <summary>
+    /// 数据列表服务
+    /// </summary>
+    public interface IDataListService
+    {
+
+    }
+}
