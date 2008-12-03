715cb422147212c754963796b7a2c60b41c72d91
Sjteksoft <Sjteksoft@fbfec8fb-a053-0410-97e8-c5a9e5c66af0>
2008-12-03 16:26:32 +0000

    为所有DevExpress可打印控件添加打印功能。

---
+++ sources/Uniframework.Client/Communicator/CallbackHandler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading;
+using Uniframework.Services;
+
+namespace Uniframework.Client
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [CallbackBehavior(ConcurrencyMode = ConcurrencyMode.Multiple, UseSynchronizationContext = false)]
+    public class CallbackHandler : IWcfCallback
+    {
+        private SynchronizationContext syncContext = null;
+
+        public void CallBackInvoke(ChatData data)
+        {
+            //ClientEventDispatcher.Instance.RaiseChatEvent(data);
+        }
+
+        /// <summary>
+        /// Notifies the data change.
+        /// </summary>
+        /// <param name="eventResult">The event result.</param>
+        public void NotifyDataChange(EventResultData eventResult)
+        {
+            EventResultData data = eventResult;
+            if (data != null) {
+                ClientEventDispatcher.Instance.DispachEvent(data.Topic, data.Args);
+            }
+        }
+
+        /// <summary>
+        /// Pings this instance.
+        /// </summary>
+        /// <returns></returns>
+        public bool Ping()
+        {
+            return true;
+        }
+    }
+}
