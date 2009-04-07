00ebe1cc1ef4d32a4b69c78b88b5095a3a740bd5
Sjteksoft <Sjteksoft@fbfec8fb-a053-0410-97e8-c5a9e5c66af0>
2009-04-07 14:33:21 +0000

    1、升级数据库存取组件； 2、升级DevExpress组件到8.36，在出产品之前此组件不再升级。

---
+++ trunk/sources/Uniframework.Database/Definition/DbBaseEntity.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Lephone.Data.Definition;
+
+namespace Uniframework.Database.Definition
+{
+    /// <summary>
+    /// 业务实体
+    /// </summary>
+    /// <typeparam name="T">实体类型</typeparam>
+    /// <typeparam name="TKey">业务实体主键类型</typeparam>
+    [Serializable]
+    public abstract class DbBaseEntity<T, TKey> : DbObjectBase where T : DbBaseEntity<T, TKey>
+    {
+        [Length(128)]
+        public abstract string Name { get; set; }
+        [Length(30)]
+        public abstract string CreatedBy { get; set; }
+        [SpecialName]
+        public abstract DateTime CreatedOn { get; set; }
+        [AllowNull, Length(30)]
+        public abstract string UpdatedBy { get; set; }
+        [SpecialName]
+        public abstract DateTime? UpdatedOn { get; set; }
+        [SpecialName]
+        public abstract int LockVersion { get; set; }
+        [AllowNull, Length(255)]
+        public abstract string Comment { get; set; }
+
+        /// <summary>
+        /// 以特定的名称实例化业务实体
+        /// </summary>
+        /// <param name="name">名称</param>
+        /// <returns>已经初始化的业务实体</returns>
+        public DbBaseEntity<T, TKey> Init(string name) {
+            this.Name = name;
+            return this;
+        }
+
+        /// <summary>
+        /// 以特定的名称及创建者实例化业务实体
+        /// </summary>
+        /// <param name="name">名称</param>
+        /// <param name="createdBy">创建者标识</param>
+        /// <returns>已经初始化的业务实体</returns>
+        public DbBaseEntity<T, TKey> Init(string name, string createdBy)
+        {
+           this.Name = name;
+           this.CreatedBy = createdBy;
+           this.UpdatedBy = createdBy;
+           return this;
+        }
+    }
+
+    /// <summary>
+    /// 业务实体类
+    /// </summary>
+    /// <typeparam name="T">实体类型</typeparam>
+    [Serializable]
+    public abstract class DbBaseEntity<T> : DbObjectModel<T, long> where T : DbObjectModel<T, long>
+    {
+        [Length(128)]
+        public abstract string Name { get; set; }
+        [Length(30)]
+        public abstract string CreatedBy { get; set; }
+        [SpecialName]
+        public abstract DateTime CreatedOn { get; set; }
+        [AllowNull, Length(30)]
+        public abstract string UpdatedBy { get; set; }
+        [SpecialName]
+        public abstract DateTime? UpdatedOn { get; set; }
+        [SpecialName]
+        public abstract int LockVersion { get; set; }
+        [AllowNull, Length(255)]
+        public abstract string Comment { get; set; }
+
+        /// <summary>
+        /// 以特定的名称实例化业务实体
+        /// </summary>
+        /// <param name="name">名称</param>
+        /// <returns>已经初始化的业务实体</returns>
+        public DbBaseEntity<T> Init(string name)
+        {
+            this.Name = name;
+            return this;
+        }
+
+        /// <summary>
+        /// 以特定的名称及创建者实例化业务实体
+        /// </summary>
+        /// <param name="name">名称</param>
+        /// <param name="createdBy">创建者标识</param>
+        /// <returns>已经初始化的业务实体</returns>
+        public DbBaseEntity<T> Init(string name, string createdBy)
+        {
+            this.Name = name;
+            this.CreatedBy = createdBy;
+            this.UpdatedBy = createdBy;
+            return this;
+        }
+    }
+}
---
+++ trunk/tests/Uniframework.Database.Tests/CommonTest.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Uniframework.Database.Definition;
+
+using Lephone.Data;
+using Lephone.Data.Definition;
+using NUnit.Framework;
+
+namespace Uniframework.Database.Tests
+{
+    [Serializable]
+    [DbTable("COM_Role")]
+    public abstract class Role : DbBaseEntity<Role>
+    {
+        [Length(30)]
+        [Index(IndexName="IX_Role", ASC=true)]
+        public abstract string Name { get; set; }
+    }
+
+    [Serializable]
+    [DbTable("COM_Unit")]
+    public abstract class Unit : DbBaseEntity<Unit, string>
+    {
+        [DbKey(IsDbGenerate = false), Length(30)]
+        public string UnitId { get; set; }
+        [Length(30)]
+        public override string Name { get; set; }
+        [SpecialName]
+        public abstract int Count { get; set; }
+
+        public Unit() { }
+
+        public Unit(string unitId) {
+            this.UnitId = unitId;
+        }
+    }
+
+    [Serializable]
+    public abstract class Person : DbObjectModel<Person>
+    {
+        [HasOne]
+        public abstract PersonalComputer PC { get; set; }
+        [Length(50)]
+        public abstract string Name { get; set; }
+    }
+
+    [DbTable("PCs")]
+    public abstract class PersonalComputer : DbObjectModel<PersonalComputer>
+    {
+        [Length(50)]
+        public abstract string Name { get; set; }
+        [BelongsTo]
+        public abstract Person Owner { get; set; }
+    }
+
+    [TestFixture]
+    public class CommonTest
+    {
+        [Test]
+        public void Test1()
+        {
+            Role role = Role.New();
+            role.Name = "Jacky";
+            role.CreatedBy = "Jacky";
+            role.UpdatedBy = "Jacky";
+            DbEntry.Save(role);
+
+            var r = DbEntry.GetObject<Role>(role.Id);
+            Assert.AreEqual(role.Name, r.Name);
+
+            role.Name = "Niuniu";
+            DbEntry.Save(role);
+            Assert.AreEqual(role.Id, r.Id);
+
+            long id = r.Id;
+            DbEntry.Delete(role);
+            var r1 = DbEntry.GetObject<Role>(id);
+            Assert.IsNull(r1);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            //Role r = new Role() { 
+            //    Name = "Jacky",
+            //    CreatedBy = "Admin",
+            //    UpdatedBy = "Admin"
+            //};
+            Role r = Role.New();
+            r.Name = "Jacky";
+            r.CreatedBy = "Admin";
+            r.UpdatedBy = "Admin";
+
+            DbEntry.Save(r);
+
+            Role r1 = DbEntry.GetObject<Role>(r.Id);
+            Assert.IsNotNull(r1);
+            Assert.AreEqual("Admin", r1.CreatedBy);
+            r1.Name = "Modified by jacky.";
+            DbEntry.Update(r1);
+
+            Role r2 = DbEntry.GetObject<Role>(r.Id);
+            Assert.AreEqual("Modified by jacky.", r2.Name);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            DbEntry.Delete<Unit>(WhereCondition.EmptyCondition);
+
+            Unit u = DynamicObject.NewObject<Unit>("00001");
+            u.Name = "吨";
+            u.CreatedBy = "Jacky";
+            u.UpdatedBy = "Admin";
+
+            DbEntry.Save(u);
+            var u1 = DbEntry.GetObject<Unit>(u.UnitId);
+            Assert.AreEqual(u1.UnitId, u.UnitId);
+
+            u1.Name = "M3";
+            DbEntry.Update(u1);
+
+            Unit u2 = DynamicObject.NewObject<Unit>("00002");
+            u2.Init("M3", "Admin");
+            Assert.AreEqual("M3", u2.Name);
+            DbEntry.Save(u2);
+
+            Assert.AreEqual(2, DbEntry.From<Unit>().Where(WhereCondition.EmptyCondition).GetCount());
+        }
+
+        [Test]
+        public void Test4()
+        {
+            Person p = Person.New();
+            p.Name = "Jacky";
+            p.PC = PersonalComputer.New();
+            p.PC.Name = "hp dl380 g3";
+            DbEntry.Save(p);
+        }
+    }
+}
